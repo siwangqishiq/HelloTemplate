cb3192b2637ba1fe7a76199561fc8cc0bba7bd60
yixin.panyi <panyi@yixin.im>
2019-08-16 16:17:55 +0800

    分文件 引入加载shader的工具方法

--- HelloTemplate.cpp
+++ HelloTemplate.cpp
@@ -6,51 +6,17 @@
 #define GLEW_STATIC  
 #include <GL/glew.h>
 #include <GL/glut.h>
-
 #include <GLFW/glfw3.h>
 
 #include "linmath.h"
+#include "App.h"
+#include "Run.hpp"
 
 using namespace std;
 
 // Function prototypes    
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-// Window dimensions    
-const GLuint WIDTH = 800, HEIGHT = 600;
-
-static struct Vertex
-{
-	float x, y;
-	float r, g, b;
-};
-
-Vertex vertices[3] =
-{
-	{ -0.6f, -0.4f, 1.f, 0.f, 0.f },
-	{ 0.6f, -0.4f, 0.f, 1.f, 0.f },
-	{ 0.f,  0.6f, 0.f, 0.f, 1.f }
-};
-
-static const char* vertex_shader_text =
-"#version 110\n"
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
-
-static const char* fragment_shader_text =
-"#version 110\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(1.0f , 0.0f , 1.0f, 1.0f);\n"
-"}\n";
 
 int main()
 {
@@ -64,11 +30,8 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
 	// Create a GLFWwindow object that we can use for GLFW's functions    
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL v1.0", nullptr, nullptr);
-	//GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", glfwGetPrimaryMonitor(), NULL);
-
-	GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-	GLint mvp_location, vpos_location, vcol_location;
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World", nullptr, nullptr);//窗口
+	//GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", glfwGetPrimaryMonitor(), NULL); //全屏
 
 	if (window == nullptr)
 	{
@@ -91,51 +54,24 @@ int main()
 		return -1;
 	}
 
-	glGenBuffers(1, &vertex_buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-	glCompileShader(vertex_shader);
-	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-	glCompileShader(fragment_shader);
-	program = glCreateProgram();
-	glAttachShader(program, vertex_shader);
-	glAttachShader(program, fragment_shader);
-	glLinkProgram(program);
-	mvp_location = glGetUniformLocation(program, "MVP");
-	vpos_location = glGetAttribLocation(program, "vPos");
-	vcol_location = glGetAttribLocation(program, "vCol");
-
 	// Define the viewport dimensions    
 	glViewport(0, 0, WIDTH, HEIGHT);
 
-	glEnableVertexAttribArray(vpos_location);
-	glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-		sizeof(vertices[0]), (void*)0);
-	glEnableVertexAttribArray(vcol_location);
-	glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-		sizeof(vertices[0]), (void*)(sizeof(float) * 2));
+	MainRun mainRun;
+	mainRun.init();
 
 	// Game loop    
 	while (!glfwWindowShouldClose(window))
 	{
 		float ratio;
 		int width, height;
-		mat4x4 m, p, mvp;
+		
 		glfwGetFramebufferSize(window, &width, &height);
 		ratio = width / (float)height;
 		glViewport(0, 0, width, height);
 		glClear(GL_COLOR_BUFFER_BIT);
-		mat4x4_identity(m);
-		mat4x4_rotate_Z(m, m, (float)glfwGetTime());
-		mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-		mat4x4_mul(mvp, p, m);
-		glUseProgram(program);
-		glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		mainRun.update(width , height);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -148,8 +84,7 @@ int main()
 }
 
 // Is called whenever a key is pressed/released via GLFW    
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode){
 	std::cout << key << std::endl;
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
